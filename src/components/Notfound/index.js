90083d75fece586dcdd73f4a352a061c4498b2fa
MadhuReddyH <hmadhusudhanreddy@gmail.com>
2023-05-23 10:49:48 +0000

    ebankCCBP

--- src/components/Notfound/index.js
+++ src/components/Notfound/index.js
@@ -1,11 +1,16 @@
+import './index.css'
+
 const NotFound = () => (
-  <div>
+  <div className="not-con">
     <img
+      className="not-img"
       src="https://assets.ccbp.in/frontend/react-js/ebank-not-found-img.png"
       alt="not found"
     />
-    <h1>Page Not Found</h1>
-    <p>We are sorry, the page you requested could not be found</p>
+    <h1 className="not-heading">Page Not Found</h1>
+    <p className="not-p">
+      We are sorry, the page you requested could not be found
+    </p>
   </div>
 )
 
